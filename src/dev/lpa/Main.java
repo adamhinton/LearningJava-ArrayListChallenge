4de850c13d2a0555bb575e7244bb2cdb30882187
adamd <adam.d.hinton@gmail.com>
2024-02-29 21:05:59 -0500

    exit option

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while(true){
+
+
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Enter 0 to exit, 1 to add items, 2 to remove items");
+            int input = scanner.nextInt();
+
+            if(input == 0){
+                break;
+            }
+
+
+
         }
+
+
     }
 }
+
+// make interactive console prog
+
+// Avail axns:
+// 0. Shut down
+// 1. Add item(s) to list (comma delimited)
+// Remove items (comma delimited list)
+
+// `enter a number for which axn you want:`
+
+// use Scanner (obviously)
+
+// This is a grocery list. Should be an arrayList
+// Use methods on ArrayList to add, remove, check if item exists, and print sorted list
+
+// Print alph sorted list after each op
+// No dupes
